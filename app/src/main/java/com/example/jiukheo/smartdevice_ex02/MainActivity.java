6937b5dc57b870400531cefd3b14317f78b28a91
Heojiuk <jwheo92@gmail.com>
2018-10-02 16:02:07 +0900

    smartdevice_ex02

---
+++ app/src/main/java/com/example/jiukheo/smartdevice_ex02/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.jiukheo.smartdevice_ex02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

4dd181fae76005af2134a96ac170634f0c31ca02
Heojiuk <jwheo92@gmail.com>
2018-10-02 16:12:11 +0900

    add Homepage

--- app/src/main/java/com/example/jiukheo/smartdevice_ex02/MainActivity.java
+++ app/src/main/java/com/example/jiukheo/smartdevice_ex02/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.jiukheo.smartdevice_ex02;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    protected Button btHomepage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btHomepage = (Button) findViewById(R.id.button);
+
+        btHomepage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*
+                그냥 = 하는거는 이미 생성된 것을 참조(레퍼런드)하기 위해서 가져오는 것이다.
+                new 하는것은 지금 새로 사용하기 위해 만드는것이다.
+                */
+
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://ice.mokwon.ac.kr"));
+                startActivity(intent);
+            }
+        });
     }
 }
